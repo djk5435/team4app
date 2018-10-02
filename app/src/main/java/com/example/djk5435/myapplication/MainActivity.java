0896bd1e248faaf3bd18709018780ec6c711fe57
mh9635 <mh9635@rit.edu>
2018-10-02 15:48:24 -0400

    updated before class ended

--- app/src/main/java/com/example/djk5435/myapplication/MainActivity.java
+++ app/src/main/java/com/example/djk5435/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.djk5435.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
