c17c18970502563e51181d1f6955fa39e522c592
mjj9707 <mjj9707@rit.edu>
2018-10-02 15:49:04 -0400

    tutorial 5

---
+++ app/src/main/java/com/example/djk5435/myapplication/Activity_mjj9707_2.java
@@ -0,0 +1,13 @@
+package com.example.djk5435.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_mjj9707_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mjj9707_2);
+    }
+}
