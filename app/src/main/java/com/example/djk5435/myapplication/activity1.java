8d3bcdf2e942fa15c319b64df1dac9665b51d4b4
Michael <mh9635@rit.edu>
2018-10-13 20:35:13 -0400

    updated shit

--- app/src/main/java/com/example/djk5435/myapplication/activity1.java
+++ app/src/main/java/com/example/djk5435/myapplication/activity1.java
@@ -7,24 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class activity1 extends AppCompatActivity {
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_1);
 
-        button = (Button) findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openactivity1();
+            public void onClick(View v){
+                openMainActivity();
             }
         });
     }
-    public void openactivity1() {
-        Intent intent = new Intent (MainActivity.this, activity2.class);
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
